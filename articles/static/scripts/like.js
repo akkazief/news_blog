d806845d8368c60f792dd4d3043807e134c2a857
akkaziev_aktan <salizardfnite@gmail.com>
2026-07-21 16:08:29 +0600

    fixing templates

--- articles/static/scripts/like.js
+++ articles/static/scripts/like.js
@@ -6,13 +6,13 @@ async function makeRequest(url, method = "GET") {
 
 async function onClick(event) {
     event.preventDefault();
-    let link = event.target;
+    let link = event.target.closest('[data-key="likes"]');
     let counterId = link.dataset.counterId;
     let counter = document.getElementById(counterId);
     let url = link.href;
     let response = await makeRequest(url);
     counter.innerText = response.count;
-    link.innerText = response.liked ? 'Убрать лайк' : 'Лайк';
+    link.innerHTML = response.liked ? '<i class="fa-solid fa-heart"></i>' : '<i class="fa-regular fa-heart"></i>';
 }
 
 function onLoad() {
